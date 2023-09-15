479848d2d2607dc6b2cab67e5d758ae0c7aa6e53
isandy8 <113350985+isandy8@users.noreply.github.com>
2023-09-15 22:00:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/screens/categories.dart';
+import 'package:meal_app/screens/meals.dart';
+import 'package:meal_app/screens/tabs.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed( brightness : Brightness.dark , seedColor: Color.fromARGB(
+      255, 140, 60, 2),),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main(){
+  runApp(ProviderScope(child: App()));
+}
+
+class App extends StatelessWidget{
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: TabsScreen(),
+
+    );
+  }
+
+}
